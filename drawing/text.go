aca6b53f8bd1f63217eb2d72e1e09e59b29a5c28
noppikinatta <noppikitouroku@gmail.com>
2024-06-21 08:16:46 +0900

    refactor: use HLineGap as line spacing

--- drawing/text.go
+++ drawing/text.go
@@ -18,25 +18,12 @@ func textImage(txt string, fontSize float64) *ebiten.Image {
 		return img
 	}
 	face := font.FontFace(fontSize)
-	w, h := text.Measure(txt, face, lineSpacing(fontSize))
+	w, h := text.Measure(txt, face, face.Metrics().HLineGap)
 	img = ebiten.NewImage(int(w+1), int(h+1))
 	opt := text.DrawOptions{}
 	text.Draw(img, txt, face, &opt)
 	textCache[k] = img
 	return img
-}
-
-func lineSpacing(fontSize float64) float64 {
-	spacing, ok := lineSpacingCache[fontSize]
-	if ok {
-		return spacing
-	}
-
-	face := font.FontFace(fontSize)
-	_, h := text.Measure("A", face, 0)
-	spacing = h * 1.5
-	lineSpacingCache[fontSize] = spacing
-	return spacing
 }
 
 type textKey struct {
@@ -46,9 +33,6 @@ type textKey struct {
 
 var textCache map[textKey]*ebiten.Image
 
-var lineSpacingCache map[float64]float64
-
 func init() {
 	textCache = make(map[textKey]*ebiten.Image)
-	lineSpacingCache = make(map[float64]float64)
 }
