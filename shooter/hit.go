b6ce972ee3a0f354b74cf15274800a1c222d34bb
noppikinatta <noppikitouroku@gmail.com>
2024-06-23 12:26:09 +0900

    feat: hittest interface

---
+++ shooter/hit.go
@@ -0,0 +1,30 @@
+package shooter
+
+import "github.com/noppikinatta/ebitenginejam03/geom"
+
+type Bullet interface {
+	IsLiving() bool
+	HitProcess(targets []Target)
+}
+
+type Target interface {
+	HitCircle() geom.Circle
+	IsEnemy() bool
+	Damage(value int)
+	IsLiving() bool
+}
+
+type HitTest struct {
+	Bullets []Bullet
+	Targets []Target
+}
+
+func (h *HitTest) Update() {
+	for _, b := range h.Bullets {
+		if !b.IsLiving() {
+			continue
+		}
+
+		b.HitProcess(h.Targets)
+	}
+}
