bab6ec50a82c00720c839bd0067d2cef66b3f8bb
noppikinatta <noppikitouroku@gmail.com>
2024-06-23 12:26:37 +0900

    feat: impl enemy

--- shooter/enemy.go
+++ shooter/enemy.go
@@ -1,14 +1,21 @@
 package shooter
 
-import "github.com/noppikinatta/ebitenginejam03/geom"
+import (
+	"math"
+	"math/rand/v2"
+
+	"github.com/noppikinatta/ebitenginejam03/geom"
+)
 
 type Enemy struct {
-	HP       int
-	State    State
-	Hit      geom.Circle
-	Velocity geom.PointF
-	Rotate   float64
-	Bullets  []*EnemyBullet
+	HP               int
+	State            State
+	Hit              geom.Circle
+	Velocity         geom.PointF
+	ShootingInterval int
+	CurrentWait      int
+	Bullets          []*EnemyBullet
+	Rnd              *rand.Rand
 }
 
 func (e *Enemy) Update() {
@@ -22,11 +29,83 @@ func (e *Enemy) Update() {
 	}
 
 	e.Hit.Center = e.Hit.Center.Add(e.Velocity)
-	// TODO: update bullets
+	e.shoot()
+}
+
+func (e *Enemy) shoot() {
+	if e.CurrentWait > 0 {
+		e.CurrentWait--
+		return
+	}
+
+	shot := false
+	for _, b := range e.Bullets {
+		b.Update()
+		if b.IsLiving() {
+			continue
+		}
+
+		if !shot {
+			b.Shoot(e.bulletInitParams())
+			shot = true
+		}
+	}
+
+	interval := e.ShootingInterval
+	if shot {
+		interval /= 4
+	}
+
+	e.CurrentWait = interval
+}
+
+func (e *Enemy) bulletInitParams() (start, velocity geom.PointF) {
+	angle := e.Velocity.Angle()
+	angle += (e.Rnd.Float64()*10 - 5) * math.Pi / 180
+	abs := e.Velocity.Abs() * 4
+
+	return e.Hit.Center, geom.PointFFromPolar(abs, angle)
 }
 
 type EnemyBullet struct {
+	Power    int
 	Hit      geom.Circle
 	Velocity geom.PointF
 	State    State
+}
+
+func (b *EnemyBullet) Shoot(start, velocity geom.PointF) {
+	b.State = StateOnStage
+	b.Hit.Center = start
+	b.Velocity = velocity
+}
+
+func (b *EnemyBullet) Update() {
+	if !b.IsLiving() {
+		return
+	}
+	b.Hit.Center = b.Hit.Center.Add(b.Velocity)
+}
+
+func (b *EnemyBullet) IsLiving() bool {
+	return b.State == StateOnStage
+}
+
+func (b *EnemyBullet) HitProcess(targets []Target) {
+	for _, t := range targets {
+		if !t.IsLiving() {
+			continue
+		}
+		if t.IsEnemy() {
+			continue
+		}
+
+		if !b.Hit.IntersectsWith(t.HitCircle()) {
+			continue
+		}
+
+		t.Damage(b.Power)
+		b.State = StateReady
+		break
+	}
 }
